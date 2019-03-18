65edf5b989b9b319bc0033acb8e5fb74698b99e0
lmarchesani <47092380+lmarchesani@users.noreply.github.com>
2019-03-18 14:00:33 -0500

    Add files via upload

---
+++ Pseudo_code.c
@@ -0,0 +1,49 @@
+includes 
+
+int main()
+//variables i will create
+int to store user choice
+prompt user whether they want the whole table or a specific number
+error checking
+switch case depending on userChoice
+1: print the whole table
+-use a for loop to iterate to 100
+-pass in the variable i to holyRomanEmpire
+2: print a specific number
+-prompt the user for the number they want  passed into holy holyRomanEmpire
+-error checking
+-pass it into holyRomanEmpire
+3:Convert roman numeral to decimal
+-prompt user for input
+-error checking
+-pass user input into Germania
+-does some math and basically just passes the elements in one by one until there is a null character and then adds them and assigns them to numbers
+default kill program
+
+
+
+
+Germania:
+Will check the values of the letter thats pushed in and pass it back in value
+
+HolyRomanEmpire
+prints the original number from user input
+checks if is 100
+-print c
+checks if it is a 90
+-prints XC
+checks if it needs an l (50)
+-prints l
+checks if it is a 40 
+-prints XL   
+while loop for how many xs it needs (10)
+-prints x
+checks if it is a 9
+-prints IX
+checks if it needs a v (5)
+-prints V
+checks if it is a 4
+-prints IV
+while loop for how many is it needs (1)
+-prints I
+prints a new line
